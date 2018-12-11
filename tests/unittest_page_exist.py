22bcf076da918b2c999f12be5307287ca9c4522e
Jingjing Zheng <jingjingzheng@g.harvard.edu>
2018-12-11 14:17:06 -0500

    add content verification from a specific tag

--- tests/unittest_page_exist.py
+++ tests/unittest_page_exist.py
@@ -16,12 +16,46 @@ except ImportError:
 class Test(unittest.TestCase):
     def setUp(self): 
         self.response_code = get_status_code(url)
+        self.response_data = get_data_from_tag(url)
 
-    def test_pass(self):
+    def test_status_code(self):
         self.assertEqual(self.response_code, 200)
 
+    def test_content(self):
+        self.assertEqual(self.response_data, '3.7.1 Documentation')
+
+# define a base class to handle all vary test cases
+class MyHTMLParser(HTMLParser):
+
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.recording = 0
+        self.data = ''
+        self.recordtitle = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'title':
+            self.recordtitle = True
+            print "Encountered the beginning of a %s tag" % tag
+            # for name, value in attrs:
+                # print name, value
+                # if name == 'class' and value == 'body':
+                #     print name, value
+                #     print "Encountered the beginning of a %s tag" % tag
+
+    def handle_endtag(self, tag):
+        if tag == 'title':
+          self.recording -=1
+          self.recordtitle = False
+          print "Encountered the end of a %s tag" % tag 
+
+    def handle_data(self, data):
+        if self.recordtitle:
+            print "Data     :", data
+            self.data = data
+
 #### function to get html status code
-###### 
+######
 
 def get_status_code(url):
     try:
@@ -39,14 +73,33 @@ def get_status_code(url):
         print(e, 'while fetching', url)
         return 
 
+#### function to get html data
+###### 
+
+def get_data_from_tag(url):
+    try:
+
+        ######## Getting URLs
+        response = urllib2.urlopen(url)
+        # debug - full html pull-down here
+        ## Get the URL. This gets the real URL. 
+        print "The URL is: ", response.geturl()
+        ## Getting the code
+        print "This gets the code: ", response.code
+        # Get all data
+        html = response.read()
+        # print "Get all data: ", html
+    except urllib2.HTTPError as e:
+        print(e, 'while fetching', url)
+        return
+
+    parser = MyHTMLParser()
+    parser.feed(html)
+    content = parser.data
+    print parser.close()
+    return content
+
 if __name__ == "__main__":
 
-    # url from stdin
-#    if len(sys.argv) != 2:
-#        print 'Usage: {} URL'.format(sys.argv[0])
-
-
-#    url = sys.argv[1]
-#    print url, "\n"
     # start unit test
     unittest.main()
