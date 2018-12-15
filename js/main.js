c908337b6673e7213b5ef8871b018cc58444c1e8
Elias Falconi <elf073@g.harvard.edu>
2018-12-15 00:22:00 -0500

    added remainder of footer css, made changes to html, added some js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-// Test
+$(document).ready(function(){  
+    $("#abt").click(function(e) {
+        
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 2000);
+    });
+    $("#svcs").click(function(e) {
+        
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 2000);
+        });
+
+    $("#pkgs").click(function(e) {
+        
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 2000);
+    });
+  });
