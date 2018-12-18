abc8fa377d8ac9b571e2c4ac4034ffbc5d8daef3
root <root@final-prod-server.us-west2-a.c.final-project-224917.internal>
2018-12-18 02:03:03 +0000

    fix update

--- Prod_Server_Files/ProdContSetup.sh
+++ Prod_Server_Files/ProdContSetup.sh
@@ -6,10 +6,9 @@ yum install git -y
 yum install docker -y
 
 #Diable and remove previous container and its image
-docker stop prod
-docker rm prod
-docker rmi prod_apache_container
-docker rmi seasadmin/centos
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
 rm -r -f /tmp/build-docker-image
 
 #Delete the old public Github repo and download the latest version
@@ -22,16 +21,12 @@ systemctl start docker
 mkdir /tmp/build-docker-image && cd /tmp/build-docker-image
 
 #Get website files from the cloned repo
-cp /root/csci-91-final-phase1-public/index.html /tmp/build-docker-image/
-cp /root/csci-91-final-phase1-public/favicon.ico /tmp/build-docker-image/
-cp /root/csci-91-final-phase1-public/404.html /tmp/build-docker-image/
-cp -r /root/csci-91-final-phase1-public/css/ /tmp/build-docker-image/
-cp -r /root/csci-91-final-phase1-public/img/ /tmp/build-docker-image/
-cp -r /root/csci-91-final-phase1-public/js/ /tmp/build-docker-image/
+cp -r /root/csci-91-final-phase1-public/ /tmp/build-docker-image/
 
 #Get DockerFile from cloned repo
-cp /root/csci-91-final-phase1-public/Prod_Server_Files/Dockerfile /tmp/build-docker-image/
+#cp /root/csci-91-final-phase1-public/Prod_Server_Files/Dockerfile /tmp/build-docker-image/
+cp /root/Dockerfile /tmp/build-docker-image/
 
 #Build the docker container using the Dockerfile and index.html files
 cd /tmp/build-docker-image && docker build -t prod_apache_container .
-docker run --name prod -d -p 2020:80 prod_apache_container
+docker run --name prod -d -p 80:80 prod_apache_container
